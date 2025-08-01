90784148702283c3e15bdefdf53d5fb68c64c174
Deva452210 <devakarun05@gmail.com>
2025-08-01 14:35:18 +0530

    Corrected the path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import SideGigs from "../components/Sidegigs";
+import SideGigs from "../components/SideGigs";
 import Collection from "../components/Collection";
 
 const Home = () => {
