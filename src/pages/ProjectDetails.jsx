dc457b7ae7996b8542cc650e2b16e12d84980955
Deva452210 <devakarun05@gmail.com>
2025-08-02 13:32:35 +0530

    Added back button in details page

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton";
 
 const ProjectDetails = () => {
   const location = useLocation();
@@ -21,33 +22,40 @@ const ProjectDetails = () => {
   }
 
   return (
-    <section
-      className=" pt-32 py-20 px-6 max-w-4xl mx-auto"
-      style={{
-        backgroundColor: "var(--background-color-lite)",
-        color: "var(--text-color)",
-      }}
-    >
-      <img
-        src={project.image}
-        alt={project.title}
-        className="w-full h-fit object-cover rounded-md mb-6"
-      />
-      <h1 className="text-3xl font-bold mb-3">{project.title}</h1>
-      <p className="text-md mb-4">{project.desc}</p>
-
-      <h3 className="text-xl font-semibold mb-2">Tech Stack</h3>
-      <div className="flex flex-wrap gap-2 mb-6">
-        {project.stack.map((tech, i) => (
-          <span
-            key={i}
-            className="bg-gray-200 text-gray-800 px-3 py-1 text-sm rounded-full"
-          >
-            {tech}
-          </span>
-        ))}
+    <>
+      <div className="max-w-4xl pt-28 py-10 px-6  mx-auto">
+        {/* Back Button on the left */}
+        <BackButton />
       </div>
-    </section>
+
+      <section
+        className=" pt-0 py-20 px-6 max-w-4xl mx-auto"
+        style={{
+          backgroundColor: "var(--background-color-lite)",
+          color: "var(--text-color)",
+        }}
+      >
+        <img
+          src={project.image}
+          alt={project.title}
+          className="w-full h-fit object-cover rounded-md mb-6"
+        />
+        <h1 className="text-3xl font-bold mb-3">{project.title}</h1>
+        <p className="text-md mb-4">{project.desc}</p>
+
+        <h3 className="text-xl font-semibold mb-2">Tech Stack</h3>
+        <div className="flex flex-wrap gap-2 mb-6">
+          {project.stack.map((tech, i) => (
+            <span
+              key={i}
+              className="bg-gray-200 text-gray-800 px-3 py-1 text-sm rounded-full"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/pages/SideGigsDetails.jsx
+++ src/pages/SideGigsDetails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import data from "../data/data.json";
+import BackButton from "../components/BackButton";
 
 const SideGigDetails = () => {
   const { id } = useParams();
@@ -15,33 +16,39 @@ const SideGigDetails = () => {
   }
 
   return (
-    <section
-      className="pt-32 px-4 py-16 max-w-4xl mx-auto"
-      style={{
-        backgroundColor: "var(--background-color-lite)",
-        color: "var(--text-color)",
-      }}
-    >
-      <h1 className="text-4xl font-bold mb-6">{gig.title}</h1>
-      <img
-        src={gig.image}
-        alt={gig.title}
-        className="w-full rounded-lg mb-6 object-cover"
-      />
-      <p className="text-lg italic text-gray-400 mb-4">{gig.subtitle}</p>
-      <div className="text-sm mb-6">{gig.description}</div>
-
-      <div className="flex flex-wrap gap-2">
-        {gig.tags.map((tag, index) => (
-          <span
-            key={index}
-            className="bg-gray-700 text-white px-3 py-1 rounded-full text-xs"
-          >
-            {tag}
-          </span>
-        ))}
+    <>
+      <div className="max-w-4xl pt-28 py-10 px-6  mx-auto">
+        {/* Back Button on the left */}
+        <BackButton />
       </div>
-    </section>
+      <section
+        className=" pt-0 py-20 px-6 max-w-4xl mx-auto"
+        style={{
+          backgroundColor: "var(--background-color-lite)",
+          color: "var(--text-color)",
+        }}
+      >
+        <h1 className="text-4xl font-bold mb-6">{gig.title}</h1>
+        <img
+          src={gig.image}
+          alt={gig.title}
+          className="w-full rounded-lg mb-6 object-cover"
+        />
+        <p className="text-lg italic text-gray-400 mb-4">{gig.subtitle}</p>
+        <div className="text-sm mb-6">{gig.description}</div>
+
+        <div className="flex flex-wrap gap-2">
+          {gig.tags.map((tag, index) => (
+            <span
+              key={index}
+              className="bg-gray-700 text-white px-3 py-1 rounded-full text-xs"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
